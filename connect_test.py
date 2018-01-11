670a25c5dcb3aadb83d248a96ea550640b0d4802
MarsiBarsi <darragon-nn@yandex.ru>
2018-01-11 22:41:54 +0300

    test

--- connect_test.py
+++ connect_test.py
@@ -4,8 +4,7 @@ from browser import document, alert
 @document["mybutton"].bind("click")
 def echo(ev):
     alert(document["zone"].value)
-    document.write("hello1")
-    document <= "Hello2"
+    document.write(array,"\n",variable)
 
 variable = 25;
 variable += 100;
@@ -16,8 +15,34 @@ for i in range (10):
     array.append(i)
 
 
-document <= variable,"\n"
-document <= "=======\n"
-document <= array
-document <= "========"
-document.write(array)
+moving = document["rot15"]
+x = 0
+dx = 3
+run = None
+
+@document['button15'].bind('click')
+def change(event):
+    global run
+    if run is None:
+        # start animation
+        animloop(1)
+    else:
+        # stop animation
+        window.cancelAnimationFrame(run)
+        run = None
+
+def render():
+    global x, dx
+    moving.style.transform = "translate({}px,0)".format(x)
+    x += dx
+    if x > document["zone15"].offsetWidth-moving.offsetWidth:
+        dx = -dx
+        moving.html = "◄" # left triangle
+    elif x <= 0:
+        dx = -dx
+        moving.html = "►" # right triangle
+
+def animloop(t):
+    global run
+    run = window.requestAnimationFrame(animloop)
+    render()
