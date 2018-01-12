631f9c1dc9b8940532c402942126e727313aea53
MarsiBarsi <darragon-nn@yandex.ru>
2018-01-12 18:35:52 +0300

    amend

--- connect_test.py
+++ connect_test.py
@@ -1,45 +1,20 @@
 from browser import document, alert
 
-moving = document["rot15"]
-x = 0
-dx = 3
-run = None
-
 @document['button15'].bind('click')
 def change(event):
-    global run
-    if run is None:
-        # start animation
-        animloop(1)
-    else:
-        # stop animation
-        window.cancelAnimationFrame(run)
-        run = None
+    array[i] += 1
+    i += 1
 
-def render():
-    global x, dx
-    moving.style.transform = "translate({}px,0)".format(x)
-    x += dx
-    if x > document["zone15"].offsetWidth-moving.offsetWidth:
-        dx = -dx
-        moving.html = "◄" # left triangle
-    elif x <= 0:
-        dx = -dx
-        moving.html = "►" # right triangle
-
-def animloop(t):
-    global run
-    run = window.requestAnimationFrame(animloop)
-    render()
-
-# bind event 'click' on button to function echo
 @document["mybutton"].bind("click")
 def echo(ev):
-    alert("rabotaet2",document["zone"].value)
     document["zone15"] <= str(array,"\n",variable)
+
+i = 1
 
 variable = 25;
 variable += 100;
+
+
 
 array = []
 
