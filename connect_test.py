e08de2fc6d310e10cf4f7945c6e112a53e63cd21
Roman Sedov <darragon-nn@yandex.ru>
2018-01-14 18:05:52 +0300

    amend

--- connect_test.py
+++ connect_test.py
@@ -1,52 +1,52 @@
 from browser import document, alert
 
-moving = document["main_cat"]
+
 
 @document['do_one'].bind('click')
 def do_one(event):
-    if array[position[0]] == 0:
-        array[position[0]] = 1
+    if band[position[0]] == 0:
+        band[position[0]] = 1
         refresh()
 
 
 @document['do_null'].bind('click')
 def do_null(event):
-    if array[position[0]] == 1:
-        array[position[0]] = 0
+    if band[position[0]] == 1:
+        band[position[0]] = 0
         refresh()
 
 @document['right'].bind('click')
 def right(event):
     position[0] += 1
-    moving.style.transform = "translate({}px,0)".format((position[0]-7)*48)
+    document["main_cat"].style.transform = "translate({}px,0)".format((position[0]-7)*46)
     refresh()
 
 @document['left'].bind('click')
 def left(event):
     position[0] -= 1
-    moving.style.transform = "translate({}px,0)".format((position[0]-7)*48)
+    document["main_cat"].style.transform = "translate({}px,0)".format((position[0]-7)*46)
     refresh()
 
 @document['end_of_program'].bind('click')
 def end_of_program(event):
-    document["zone15"].textContent = 'done'
+    document["band"].textContent = 'done'
 
 def refresh():
     out_string = array_string()
-    document["zone15"].textContent = out_string
+    document["band"].textContent = out_string
 
 def array_string():
     out_string = ' | '
-    for i in range (15):
-        out_string += str(array[i])
+    for i in range (start_position,start_position+15):
+        out_string += str(band[i])
         out_string += ' | '
     return out_string
 
+start_position = 50
 
+band = [0 for i in range (100)]
 
-array = [0 for i in range (100)]
-
-position = [7]
+position = [(start_position+7)]
 
 out_string = array_string()
 
