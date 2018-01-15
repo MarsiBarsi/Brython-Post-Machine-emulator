96096b159c92afcf32b6f95c3eb1efa21b0bcbd3
MarsiBarsi <darragon-nn@yandex.ru>
2018-01-15 12:10:46 +0300

    amend

--- connect_test.py
+++ connect_test.py
@@ -65,7 +65,10 @@ def main_cat_moving(action):
 
     if action == 'up':
         #main_cat_position[1] -= 30
-        document["main_cat"].style.transform = "translate({}px,{}px)".format(main_cat_position[0],main_cat_position[1])
+        document["main_cat"].style.transform = '''@keyframes move {
+             50% {
+             transform: translate(0px, -30px);
+          }'''.format(main_cat_position[0],main_cat_position[1])
         #main_cat_position[1] += 30
 
 start_position = [50]
