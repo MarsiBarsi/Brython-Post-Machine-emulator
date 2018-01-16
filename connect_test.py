6d5ee2d80638ccd8e3171f37e203696f563933c3
MarsiBarsi <darragon-nn@yandex.ru>
2018-01-16 16:56:32 +0300

    amend

--- connect_test.py
+++ connect_test.py
@@ -65,7 +65,7 @@ def main_cat_moving(action):
 
     if action == 'up':
         main_cat_position[1] -= 30
-        document["main_cat"].style.transform = "translateY({}px)".format(main_cat_position[1])
+        document["main_cat"].style.transform = "animation: move 0.5s ease".format(main_cat_position[1])
         main_cat_position[1] += 30
 
 start_position = [50]
